bb56994e43fb5758998c73205be6d8452533162f
Cassi <120105652+CypherO2@users.noreply.github.com>
2024-05-23 12:46:15 +0000

    resume page updated

--- site-portfolio/src/pages/Resumepage.tsx
+++ site-portfolio/src/pages/Resumepage.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import CV from "../assets/CV/UpdateCV(2)_23-05-2024.pdf";
 import { MDBContainer, MDBIcon } from "mdb-react-ui-kit";
+import { Col, Row } from "react-bootstrap";
 
 export default function Resumepage() {
   useEffect(() => {
@@ -9,11 +10,13 @@ export default function Resumepage() {
   return (
     <>
       <MDBContainer className="text-center py-2">
-
-      
-
+        <Row>
+          <Col className="d-flex justify-content-center">
+            <iframe src={CV} width="80%" height="1435vw" className="" />
+          </Col>
+        </Row>
         <a href={CV} download className="fs-1 fw-bold">
-          <span className="text-dark">For My CV:</span> Click Here!{" "}
+          <span className="text-dark">For My Preview CV:</span> Click Here!{" "}
           <MDBIcon fas className="fa-file-pdf" />
         </a>
       </MDBContainer>
