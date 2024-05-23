35940623fb7cc67f5512d79b2181ddbf571d865a
Cassi <120105652+CypherO2@users.noreply.github.com>
2024-05-23 08:45:04 +0000

    download link for CV

--- site-portfolio/src/pages/Resumepage.tsx
+++ site-portfolio/src/pages/Resumepage.tsx
@@ -1,8 +1,22 @@
 import { useEffect } from "react";
+import CV from "../assets/CV/UpdateCV(2)_23-05-2024.pdf";
+import { MDBContainer, MDBIcon } from "mdb-react-ui-kit";
 
 export default function Resumepage() {
   useEffect(() => {
     document.title = "CJ Presley | Resume";
   });
-  return <></>;
+  return (
+    <>
+      <MDBContainer className="text-center py-2">
+
+      
+
+        <a href={CV} download className="fs-1 fw-bold">
+          <span className="text-dark">For My CV:</span> Click Here!{" "}
+          <MDBIcon fas className="fa-file-pdf" />
+        </a>
+      </MDBContainer>
+    </>
+  );
 }
