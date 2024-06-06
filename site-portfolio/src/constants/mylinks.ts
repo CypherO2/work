7c798649858282e53a7898b96387a728ff0c2dd4
Cassi <120105652+CypherO2@users.noreply.github.com>
2024-06-06 12:24:15 +0000

    fiexed github link

--- site-portfolio/src/constants/mylinks.ts
+++ site-portfolio/src/constants/mylinks.ts
@@ -2,7 +2,7 @@ const DISCORD_LINK = "https://discord.gg/ZJJzEa7x6j";
 const YOUTUBE_LINK = "https://www.youtube.com/channel/UC3oX3v2LZpuB08qEGCgwcBw";
 const TWITTER_LINK = "www.twitter.com";
 const LINKEDIN_LINK = "https://www.linkedin.com/in/cjpresley/";
-const GITHUB_LINK = "https://github.comhttps://github.com/CypherO2/";
+const GITHUB_LINK = "https://github.com/CypherO2/";
 const SGATHACH_LINK = "https://github.com/CypherO2/Sg-thach-Discord-Bot";
 
 // T LEVEL LINKS
